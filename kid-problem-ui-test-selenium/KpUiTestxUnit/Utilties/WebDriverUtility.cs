e3b3aaa4a2cc838b3ee716214c2f082c726f2946
Chongtian Gao <chongtiangao@gmail.com>
2026-03-12 13:30:10 -0500

    Testing timezone for xUnit/Selenium in CI

--- kid-problem-ui-test-selenium/KpUiTestxUnit/Utilties/WebDriverUtility.cs
+++ kid-problem-ui-test-selenium/KpUiTestxUnit/Utilties/WebDriverUtility.cs
@@ -9,10 +9,18 @@ public class WebDriverUtility
     public static IWebDriver GetDriver()
     {
         ChromeOptions options = new ChromeOptions();
-        options.AddArgument("headless");  // Run the browser in headless mode for CI environments
-        options.AddArgument("--timezone=America/Chicago"); // Test data are sensitive to timezone
+        options.AddArgument("--headless");  // Run the browser in headless mode for CI environments
+        // options.AddArgument("--timezone=America/Chicago"); // Test data are sensitive to timezone; This did not work.
         IWebDriver driver = new ChromeDriver(options);
         driver.Manage().Window.Size = new System.Drawing.Size(1920, 1080);
+        try
+        {
+            ((ChromeDriver)driver).ExecuteCdpCommand("Emulation.setTimezoneOverride", new Dictionary<string, object> { { "timezoneId", "America/Chicago" } });
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
         return driver;
     }
 
